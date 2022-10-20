de2ff9f01e7c303d922944760ba254ffec2f698e
Fullajtar <fullajtar.dominik@gmail.com>
2022-10-20 19:36:20 +0200

    feat: initial basic data tranfer between fragments

---
+++ app/src/main/java/com/example/mobv_cv2/Detail.kt
@@ -0,0 +1,42 @@
+package com.example.mobv_cv2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import androidx.navigation.fragment.navArgs
+import com.example.mobv_cv2.databinding.FragmentDetailBinding
+
+class Detail : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    // get the arguments from the Registration fragment
+    private val args : DetailArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // Receive the arguments in a variable
+        val userDetails = args.user
+
+        // set the values to respective textViews
+        binding.tvName.text = userDetails.name
+        binding.tvEmail.text = userDetails.email
+        binding.tvPassword.text = userDetails.password
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/mobv_cv2/User.kt
@@ -0,0 +1,12 @@
+package com.example.mobv_cv2
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class User(
+    val name : String ="",
+    val email : String= "",
+    val password : String =""
+
+) : Parcelable
