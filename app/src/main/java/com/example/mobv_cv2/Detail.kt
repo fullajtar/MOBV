bc478831747bf6d89bc83d312ec3e6a4c35f0de2
Fullajtar <fullajtar.dominik@gmail.com>
2022-10-21 21:13:23 +0200

    feat: open google maps at coords entered in form

--- app/src/main/java/com/example/mobv_cv2/Detail.kt
+++ app/src/main/java/com/example/mobv_cv2/Detail.kt
@@ -1,13 +1,15 @@
 package com.example.mobv_cv2
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.mobv_cv2.databinding.FragmentDetailBinding
+
 
 class Detail : Fragment() {
 
@@ -30,8 +32,21 @@ class Detail : Fragment() {
         // set the values to respective textViews
         binding.tvName.text = formDetails.name
         binding.tvRestaurantName.text = formDetails.restaurantName
-        binding.tvLatitude.text = formDetails.latitude.toString()
-        binding.tvLongitude.text = formDetails.longitude.toString()
+        val latitudeString = formDetails.latitude.toString()
+        val longitudeString = formDetails.longitude.toString()
+
+        binding.tvLatitude.text = latitudeString
+        binding.tvLongitude.text = longitudeString
+
+        binding.buttonShowMap.setOnClickListener{
+            val packageManager = requireActivity().packageManager
+            val gmmIntentUri = Uri.parse("geo:$latitudeString,$longitudeString")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            mapIntent.resolveActivity(packageManager)?.let {
+                startActivity(mapIntent)
+            }
+        }
 
         return view
     }
