7f0467453bba44f6b26d5faad118b8ac3819c03f
Fullajtar <fullajtar.dominik@gmail.com>
2022-11-05 19:42:47 +0100

    demo: GameViewModel

--- app/src/main/java/com/example/mobv/fragment/Game.kt
+++ app/src/main/java/com/example/mobv/fragment/Game.kt
@@ -11,6 +11,7 @@ import com.example.mobv.databinding.FragmentGameBinding
 import com.example.mobv.databinding.FragmentListPubBinding
 import com.example.mobv.model.GameViewModel
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.fragment_game.view.*
 import java.lang.System.exit
 
 // TODO: Rename parameter arguments, choose names that match
@@ -27,6 +28,7 @@ class Game : Fragment() {
     private var _binding: FragmentGameBinding? = null
     private val binding get() = _binding!!
     private val viewModel = GameViewModel()
+    private val  MAX_NO_OF_WORDS = 50
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,55 +43,54 @@ class Game : Fragment() {
         return view
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        // Setup a click listener for the Submit and Skip buttons.
-//        binding.submit.setOnClickListener { onSubmitWord() }
-//        binding.skip.setOnClickListener { onSkipWord() }
-//        // Update the UI
-//        updateNextWordOnScreen()
-//        binding.score.text = getString(R.string.score, 0)
-//        binding.wordCount.text = getString(
-//            R.string.word_count, 0, MAX_NO_OF_WORDS)
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Setup a click listener for the Submit and Skip buttons.
+        binding.submit.setOnClickListener { onSubmitWord() }
+        binding.skip.setOnClickListener { onSkipWord() }
+        // Update the UI
+        updateNextWordOnScreen()
+        binding.score.text = viewModel.score.toString()
+        binding.wordCount.text = viewModel.currentWordCount.toString()
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
-//    /*
-//    * Checks the user's word, and updates the score accordingly.
-//    * Displays the next scrambled word.
-//    * After the last word, the user is shown a Dialog with the final score.
-//    */
-//    private fun onSubmitWord() {
-//        val playerWord = binding.textInputEditText.text.toString()
-//
-//        if (viewModel.isUserWordCorrect(playerWord)) {
-//            setErrorTextField(false)
-//            if (viewModel.nextWord()) {
-//                updateNextWordOnScreen()
-//            } else {
-//                showFinalScoreDialog()
-//            }
-//        } else {
-//            setErrorTextField(true)
-//        }
-//    }
+    /*
+    * Checks the user's word, and updates the score accordingly.
+    * Displays the next scrambled word.
+    * After the last word, the user is shown a Dialog with the final score.
+    */
+    private fun onSubmitWord() {
+        val playerWord = binding.textInputEditText.text.toString()
 
-//    /*
-//* Skips the current word without changing the score.
-//*/
-//    private fun onSkipWord() {
-//        if (viewModel.nextWord()) {
-//            setErrorTextField(false)
-//            updateNextWordOnScreen()
-//        } else {
-//            showFinalScoreDialog()
-//        }
-//    }
+        if (viewModel.isUserWordCorrect(playerWord)) {
+            setErrorTextField(false)
+            if (viewModel.nextWord()) {
+                updateNextWordOnScreen()
+            } else {
+                showFinalScoreDialog()
+            }
+        } else {
+            setErrorTextField(true)
+        }
+    }
+
+    /*
+* Skips the current word without changing the score.
+*/
+    private fun onSkipWord() {
+        if (viewModel.nextWord()) {
+            setErrorTextField(false)
+            updateNextWordOnScreen()
+        } else {
+            showFinalScoreDialog()
+        }
+    }
 
 //    /*
 // * Gets a random word for the list of words and shuffles the letters in it.
@@ -100,10 +101,10 @@ class Game : Fragment() {
 //        return String(tempWord)
 //    }
 
-//    /*
-//    * Creates and shows an AlertDialog with the final score.
-//    */
-//    private fun showFinalScoreDialog() {
+    /*
+    * Creates and shows an AlertDialog with the final score.
+    */
+    private fun showFinalScoreDialog() {
 //        MaterialAlertDialogBuilder(requireContext())
 //            .setTitle(getString(R.string.congratulations))
 //            .setMessage(getString(R.string.you_scored, viewModel.score))
@@ -115,17 +116,17 @@ class Game : Fragment() {
 //                restartGame()
 //            }
 //            .show()
-//    }
+    }
 
-//    /*
-//     * Re-initializes the data in the ViewModel and updates the views with the new data, to
-//     * restart the game.
-//     */
-//    private fun restartGame() {
-//        viewModel.reinitializeData()
-//        setErrorTextField(false)
-//        updateNextWordOnScreen()
-//    }
+    /*
+     * Re-initializes the data in the ViewModel and updates the views with the new data, to
+     * restart the game.
+     */
+    private fun restartGame() {
+        viewModel.reinitializeData()
+        setErrorTextField(false)
+        updateNextWordOnScreen()
+    }
 
     /*
      * Exits the game.
@@ -139,10 +140,10 @@ class Game : Fragment() {
         Log.d("GameFragment", "GameFragment destroyed!")
     }
 
-//    /*
-//    * Sets and resets the text field error status.
-//    */
-//    private fun setErrorTextField(error: Boolean) {
+    /*
+    * Sets and resets the text field error status.
+    */
+    private fun setErrorTextField(error: Boolean) {
 //        if (error) {
 //            binding.textField.isErrorEnabled = true
 //            binding.textField.error = getString(R.string.try_again)
@@ -150,7 +151,7 @@ class Game : Fragment() {
 //            binding.textField.isErrorEnabled = false
 //            binding.textInputEditText.text = null
 //        }
-//    }
+    }
 
     /*
      * Displays the next scrambled word on screen.
